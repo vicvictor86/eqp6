896c2bfa21ea0ade9af3ff8484479031069c19e3
Victor Emmanuel <vicbem@hotmail.com>
2023-07-24 22:56:15 -0300

    BugFix: Remove email confirmation from updateAvatar

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { container } from 'tsyringe';
 
 import { AvatarController } from '@controllers/AvatarController';
 import { UsersController } from '@controllers/UsersController';
 
 import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticate';
-import { EnsureEmailConfirmationMiddleware } from '@shared/middlewares/EnsureEmailConfirmationMiddleware';
 
 import { uploadConfig } from '@config/upload';
 
@@ -15,9 +13,6 @@ const uploadMiddleware = multer(uploadConfig.multer);
 
 const usersController = new UsersController();
 const avatarController = new AvatarController();
-const ensureEmailConfirmation = container.resolve(
-  EnsureEmailConfirmationMiddleware,
-);
 
 userRouter.get('/', usersController.show);
 userRouter.post('/', usersController.create);
@@ -25,7 +20,6 @@ userRouter.post('/', usersController.create);
 userRouter.patch(
   '/avatar',
   ensureAuthenticated,
-  ensureEmailConfirmation.execute,
   uploadMiddleware.single('avatar'),
   avatarController.update,
 );
